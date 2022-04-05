4a35d6403bfc6ed5b3ca27018989d5925b7364e3
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-04-05 13:46:58 +0700

    Add Add Dues Form

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class App extends ConsumerWidget {
 
     return MaterialApp.router(
       title: "Yuuran",
+      color: primary,
       debugShowCheckedModeBanner: false,
       theme: _theme.copyWith(
         textTheme: bFontTextTheme(context),
@@ -103,4 +104,362 @@ class MyEvent extends Equatable {
     MyEvent(title: "Event 2"),
     MyEvent(title: "Event 3"),
   ];
+}
+
+enum StatusActive { active, notActive, none }
+enum StatusPayment { paidOff, notPaidOff, none }
+
+class DuesCategory extends Equatable {
+  const DuesCategory({
+    this.id = 0,
+    this.code = '',
+    this.name = '',
+    this.description,
+    required this.status,
+    this.createdAt,
+    this.updatedAt,
+    this.createdBy,
+    this.updatedBy,
+  });
+
+  final int id;
+  final String code;
+  final String name;
+  final String? description;
+  final StatusActive status;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+  final int? createdBy;
+  final int? updatedBy;
+
+  @override
+  List<Object?> get props {
+    return [
+      id,
+      code,
+      name,
+      description,
+      status,
+      createdAt,
+      updatedAt,
+      createdBy,
+      updatedBy,
+    ];
+  }
+
+  @override
+  String toString() {
+    return 'DuesCategory(id: $id, code: $code, name: $name, description: $description, status: $status, createdAt: $createdAt, updatedAt: $updatedAt, createdBy: $createdBy, updatedBy: $updatedBy)';
+  }
+
+  DuesCategory copyWith({
+    int? id,
+    String? code,
+    String? name,
+    String? description,
+    StatusActive? status,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    int? createdBy,
+    int? updatedBy,
+  }) {
+    return DuesCategory(
+      id: id ?? this.id,
+      code: code ?? this.code,
+      name: name ?? this.name,
+      description: description ?? this.description,
+      status: status ?? this.status,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      createdBy: createdBy ?? this.createdBy,
+      updatedBy: updatedBy ?? this.updatedBy,
+    );
+  }
+}
+
+class Dues extends Equatable {
+  const Dues({
+    this.id = 0,
+    required this.citizen,
+    this.name = '',
+    required this.date,
+    this.createdAt,
+    this.updatedAt,
+    this.createdBy,
+    this.updatedBy,
+  });
+
+  final int id;
+  final User citizen;
+  final String name;
+  final DateTime date;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+  final int? createdBy;
+  final int? updatedBy;
+
+  @override
+  List<Object?> get props {
+    return [
+      id,
+      citizen,
+      name,
+      date,
+      createdAt,
+      updatedAt,
+      createdBy,
+      updatedBy,
+    ];
+  }
+
+  @override
+  String toString() {
+    return 'Dues(id: $id, citizen: $citizen, name: $name, date: $date, createdAt: $createdAt, updatedAt: $updatedAt, createdBy: $createdBy, updatedBy: $updatedBy)';
+  }
+
+  Dues copyWith({
+    int? id,
+    User? citizen,
+    String? name,
+    DateTime? date,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    int? createdBy,
+    int? updatedBy,
+  }) {
+    return Dues(
+      id: id ?? this.id,
+      citizen: citizen ?? this.citizen,
+      name: name ?? this.name,
+      date: date ?? this.date,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      createdBy: createdBy ?? this.createdBy,
+      updatedBy: updatedBy ?? this.updatedBy,
+    );
+  }
+}
+
+class DuesDetail extends Equatable {
+  const DuesDetail({
+    this.id = 0,
+    required this.dues,
+    required this.duesCategory,
+    required this.citizen,
+    this.amount = 0.0,
+    required this.status,
+    this.paidBySomeoneElse = false,
+    this.description,
+    this.createdAt,
+    this.updatedAt,
+    this.createdBy,
+    this.updatedBy,
+  });
+
+  final int id;
+  final Dues dues;
+  final DuesCategory duesCategory;
+  final User citizen;
+  final double amount;
+  final StatusPayment status;
+  final bool paidBySomeoneElse;
+  final String? description;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+  final int? createdBy;
+  final int? updatedBy;
+
+  @override
+  List<Object?> get props {
+    return [
+      id,
+      dues,
+      duesCategory,
+      citizen,
+      amount,
+      status,
+      paidBySomeoneElse,
+      description,
+      createdAt,
+      updatedAt,
+      createdBy,
+      updatedBy,
+    ];
+  }
+
+  @override
+  String toString() {
+    return 'DuesDetail(id: $id, dues: $dues, duesCategory: $duesCategory, citizen: $citizen, amount: $amount, status: $status, paidBySomeoneElse: $paidBySomeoneElse, description: $description, createdAt: $createdAt, updatedAt: $updatedAt, createdBy: $createdBy, updatedBy: $updatedBy)';
+  }
+
+  DuesDetail copyWith({
+    int? id,
+    Dues? dues,
+    DuesCategory? duesCategory,
+    User? citizen,
+    double? amount,
+    StatusPayment? status,
+    bool? paidBySomeoneElse,
+    String? description,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    int? createdBy,
+    int? updatedBy,
+  }) {
+    return DuesDetail(
+      id: id ?? this.id,
+      dues: dues ?? this.dues,
+      duesCategory: duesCategory ?? this.duesCategory,
+      citizen: citizen ?? this.citizen,
+      amount: amount ?? this.amount,
+      status: status ?? this.status,
+      paidBySomeoneElse: paidBySomeoneElse ?? this.paidBySomeoneElse,
+      description: description ?? this.description,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      createdBy: createdBy ?? this.createdBy,
+      updatedBy: updatedBy ?? this.updatedBy,
+    );
+  }
+}
+
+class AppGroupUser extends Equatable {
+  const AppGroupUser({
+    this.id = 0,
+    this.code = '',
+    this.name = '',
+    required this.status,
+    this.createdAt,
+    this.updatedAt,
+    this.createdBy,
+    this.updatedBy,
+  });
+
+  final int id;
+  final String code;
+  final String name;
+  final StatusActive status;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+  final int? createdBy;
+  final int? updatedBy;
+
+  @override
+  List<Object?> get props {
+    return [
+      id,
+      code,
+      name,
+      status,
+      createdAt,
+      updatedAt,
+      createdBy,
+      updatedBy,
+    ];
+  }
+
+  @override
+  String toString() {
+    return 'AppGroupUser(id: $id, code: $code, name: $name, status: $status, createdAt: $createdAt, updatedAt: $updatedAt, createdBy: $createdBy, updatedBy: $updatedBy)';
+  }
+
+  AppGroupUser copyWith({
+    int? id,
+    String? code,
+    String? name,
+    StatusActive? status,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    int? createdBy,
+    int? updatedBy,
+  }) {
+    return AppGroupUser(
+      id: id ?? this.id,
+      code: code ?? this.code,
+      name: name ?? this.name,
+      status: status ?? this.status,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      createdBy: createdBy ?? this.createdBy,
+      updatedBy: updatedBy ?? this.updatedBy,
+    );
+  }
+}
+
+class User extends Equatable {
+  const User({
+    this.id = 0,
+    required this.appGroupUser,
+    this.username = '',
+    this.name = '',
+    this.email = '',
+    required this.status,
+    this.profileImage,
+    this.createdAt,
+    this.updatedAt,
+    this.createdBy,
+    this.updatedBy,
+  });
+
+  final int id;
+  final AppGroupUser appGroupUser;
+  final String username;
+  final String name;
+  final String email;
+  final StatusActive status;
+  final String? profileImage;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+  final int? createdBy;
+  final int? updatedBy;
+
+  @override
+  List<Object?> get props {
+    return [
+      id,
+      appGroupUser,
+      username,
+      name,
+      email,
+      status,
+      profileImage,
+      createdAt,
+      updatedAt,
+      createdBy,
+      updatedBy,
+    ];
+  }
+
+  @override
+  String toString() {
+    return 'User(id: $id, appGroupUser: $appGroupUser, username: $username, name: $name, email: $email, status: $status, profileImage: $profileImage, createdAt: $createdAt, updatedAt: $updatedAt, createdBy: $createdBy, updatedBy: $updatedBy)';
+  }
+
+  User copyWith({
+    int? id,
+    AppGroupUser? appGroupUser,
+    String? username,
+    String? name,
+    String? email,
+    StatusActive? status,
+    String? profileImage,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    int? createdBy,
+    int? updatedBy,
+  }) {
+    return User(
+      id: id ?? this.id,
+      appGroupUser: appGroupUser ?? this.appGroupUser,
+      username: username ?? this.username,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      status: status ?? this.status,
+      profileImage: profileImage ?? this.profileImage,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      createdBy: createdBy ?? this.createdBy,
+      updatedBy: updatedBy ?? this.updatedBy,
+    );
+  }
 }
--- lib/src/utils/go_router.dart
+++ lib/src/utils/go_router.dart
@@ -1,10 +1,13 @@
 import 'dart:collection';
+import 'dart:developer';
 
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:global_template/global_template.dart';
 import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -16,9 +19,11 @@ const splashRouteName = "splash";
 const onboardingRouteName = "onboarding";
 const loginRouteName = "login";
 const welcomeRouteName = "welcome";
+const duesRouteName = "dues";
+const citizenRouteName = "citizen";
 
-final goRouter = Provider<GoRouter>((ref) {
-  return GoRouter(
+final goRouter = Provider<GoRouter>(
+  (ref) => GoRouter(
     debugLogDiagnostics: true,
     initialLocation: "/welcome",
     redirect: (state) {
@@ -45,9 +50,224 @@ final goRouter = Provider<GoRouter>((ref) {
         name: welcomeRouteName,
         builder: (ctx, state) => const WelcomePage(),
       ),
+      GoRoute(
+        path: "/dues",
+        name: duesRouteName,
+        builder: (ctx, state) => const DuesPage(),
+      ),
     ],
-  );
-});
+  ),
+);
+
+class DuesPage extends StatefulWidget {
+  const DuesPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<DuesPage> createState() => _DuesPageState();
+}
+
+class _DuesPageState extends State<DuesPage> {
+  final amountController = TextEditingController();
+  final descriptionController = TextEditingController();
+
+  String? _selectedCitizen;
+  String? _selectedDuesCategory;
+  bool _selectedPaidBySomeoneElse = false;
+
+  static const _locale = 'id';
+  String _formatNumber(String s) => NumberFormat.decimalPattern(_locale).format(int.parse(s));
+  String get _currency => NumberFormat.compactSimpleCurrency(locale: _locale).currencySymbol;
+
+  @override
+  Widget build(BuildContext context) {
+    final outlineInputBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10.0),
+      borderSide: const BorderSide(color: grey),
+    );
+    final _inputDecoration = InputDecoration(
+      filled: true,
+      fillColor: Colors.white,
+      hintText: "0",
+      hintStyle: bFont.copyWith(fontSize: 12.0, color: grey),
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder.copyWith(borderSide: const BorderSide(color: primary)),
+      contentPadding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Tambah Iuran",
+          style: hFontWhite.copyWith(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Card(
+                margin: EdgeInsets.zero,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      ...[
+                        Text(
+                          'Pilih Warga',
+                          style: hFont.copyWith(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        const SizedBox(height: 8.0),
+                        DropdownButton<String>(
+                          isExpanded: true,
+                          hint: Text(
+                            "Pilih warga yang ingin dibuatkan iuran",
+                            style: bFont.copyWith(fontSize: 12.0, color: grey),
+                          ),
+                          value: _selectedCitizen,
+                          items: ["Zeffry Reynando", "Syarif", "Helmi", "Engkoh"]
+                              .map((e) => DropdownMenuItem(child: Text(e), value: e))
+                              .toList(),
+                          onChanged: (value) => setState(() => _selectedCitizen = value),
+                        ),
+                        const SizedBox(height: 8.0),
+                      ],
+                      const SizedBox(height: 16.0),
+                      ...[
+                        Text(
+                          'Pilih Kategori Iuran',
+                          style: hFont.copyWith(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        const SizedBox(height: 8.0),
+                        DropdownButton<String>(
+                          isExpanded: true,
+                          hint: Text(
+                            "Pilih kategori iuran",
+                            style: bFont.copyWith(fontSize: 12.0, color: grey),
+                          ),
+                          value: _selectedDuesCategory,
+                          items: ["Iuran Kebersihan", "Iuran Kemanan", "Iuran Kesehatan"]
+                              .map((e) => DropdownMenuItem(child: Text(e), value: e))
+                              .toList(),
+                          onChanged: (value) => setState(() => _selectedDuesCategory = value),
+                        ),
+                        const SizedBox(height: 8.0),
+                      ],
+                      const SizedBox(height: 16.0),
+                      ...[
+                        Text(
+                          'Jumlah Iuran',
+                          style: hFont.copyWith(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        const SizedBox(height: 8.0),
+                        TextFormField(
+                          controller: amountController,
+                          style: bFont.copyWith(fontWeight: FontWeight.bold, fontSize: 12.0),
+                          keyboardType: TextInputType.number,
+                          decoration: _inputDecoration.copyWith(prefixText: _currency),
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                          onChanged: (value) {
+                            log("before value $value");
+                            if (value.isNotEmpty) value = _formatNumber(value.replaceAll(",", ""));
+
+                            log("after value value $value");
+                            amountController.value = TextEditingValue(
+                              text: value,
+                              selection: TextSelection.collapsed(offset: value.length),
+                            );
+                          },
+                        ),
+                        const SizedBox(height: 8.0),
+                      ],
+                      const SizedBox(height: 16.0),
+                      ...[
+                        CheckboxListTile(
+                          value: _selectedPaidBySomeoneElse,
+                          controlAffinity: ListTileControlAffinity.leading,
+                          contentPadding: const EdgeInsets.only(),
+                          activeColor: primary,
+                          onChanged: (value) =>
+                              setState(() => _selectedPaidBySomeoneElse = value ?? false),
+                          title: Text("Dibayarkan orang lain ?",
+                              style: hFont.copyWith(fontWeight: FontWeight.bold)),
+                          subtitle: Text(
+                            "Memberitahukan bahwa iuran ini dibayarkan oleh orang lain, bukan dengan orang yang bersangkutan.",
+                            style: bFont.copyWith(
+                              fontSize: 10.0,
+                              color: grey,
+                            ),
+                          ),
+                        ),
+                      ],
+                      const SizedBox(height: 16.0),
+                      Text(
+                        'Keterangan',
+                        style: hFont.copyWith(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                      const SizedBox(height: 8.0),
+                      TextFormField(
+                        controller: descriptionController,
+                        style: bFont.copyWith(fontWeight: FontWeight.bold, fontSize: 12.0),
+                        minLines: 3,
+                        maxLines: 3,
+                        decoration: _inputDecoration,
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        "Kamu bisa menggunakan field ini untuk memberikan keterangan pada iuran ini, bisa berupa alasan pembayaran iuran tidak full, alasan kenapa iuran dibayar oleh pihak lain, dan sebagainya.",
+                        style: bFont.copyWith(
+                          fontSize: 10.0,
+                          color: grey,
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.all(16.0),
+                          primary: primary,
+                        ),
+                        child: Text(
+                          "Submit",
+                          style: bFont.copyWith(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({
@@ -97,7 +317,7 @@ class _WelcomePageState extends State<WelcomePage> {
           children: const [
             HomePage(),
             SearchPage(),
-            AddPage(),
+            ChooseActionPage(),
             CalendarPage(),
             AccountPage(),
           ],
@@ -460,8 +680,8 @@ class _CalendarPageState extends State<CalendarPage> {
   }
 }
 
-class AddPage extends StatelessWidget {
-  const AddPage({
+class ChooseActionPage extends StatelessWidget {
+  const ChooseActionPage({
     Key? key,
   }) : super(key: key);
 
@@ -478,7 +698,7 @@ class AddPage extends StatelessWidget {
             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
             margin: EdgeInsets.zero,
             child: ListTile(
-              onTap: () {},
+              onTap: () => context.pushNamed(duesRouteName),
               contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
               leading: const CircleAvatar(
                 backgroundColor: primary,
@@ -537,7 +757,7 @@ class SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var outlineInputBorder = OutlineInputBorder(
+    final outlineInputBorder = OutlineInputBorder(
       borderRadius: BorderRadius.circular(30.0),
       borderSide: const BorderSide(color: grey),
     );
