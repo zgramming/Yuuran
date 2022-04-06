82745c1be20f2b660ccf0c6fe60e314d6c7d3e65
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-04-06 13:53:57 +0700

    Adding amount textfield on DuesCategoryFormPage

--- lib/src/utils/go_router.dart
+++ lib/src/utils/go_router.dart
@@ -235,27 +235,38 @@ class CitizenFormPage extends StatelessWidget {
   }
 }
 
-class DuesCategoryFormPage extends StatelessWidget {
+class DuesCategoryFormPage extends StatefulWidget {
   const DuesCategoryFormPage({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final outlineInputBorder = OutlineInputBorder(
-      borderRadius: BorderRadius.circular(10.0),
-      borderSide: const BorderSide(color: grey),
-    );
-    final _inputDecoration = InputDecoration(
-      filled: true,
-      fillColor: Colors.white,
-      hintText: "",
-      hintStyle: bFont.copyWith(fontSize: 12.0, color: grey),
-      enabledBorder: outlineInputBorder,
-      focusedBorder: outlineInputBorder.copyWith(borderSide: const BorderSide(color: primary)),
-      contentPadding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-    );
+  State<DuesCategoryFormPage> createState() => _DuesCategoryFormPageState();
+}
 
+class _DuesCategoryFormPageState extends State<DuesCategoryFormPage> {
+  final amountController = TextEditingController();
+  static const _locale = 'id';
+  String _formatNumber(String s) => NumberFormat.decimalPattern(_locale).format(int.parse(s));
+  String get _currency => NumberFormat.compactSimpleCurrency(locale: _locale).currencySymbol;
+
+  static final _outlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10.0),
+    borderSide: const BorderSide(color: grey),
+  );
+
+  final _inputDecoration = InputDecoration(
+    filled: true,
+    fillColor: Colors.white,
+    hintText: "",
+    hintStyle: bFont.copyWith(fontSize: 12.0, color: grey),
+    enabledBorder: _outlineInputBorder,
+    focusedBorder: _outlineInputBorder.copyWith(borderSide: const BorderSide(color: primary)),
+    contentPadding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+  );
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -304,6 +315,37 @@ class DuesCategoryFormPage extends StatelessWidget {
                     style: bFont.copyWith(fontWeight: FontWeight.bold),
                     decoration: _inputDecoration.copyWith(hintText: "Nama"),
                   ),
+                ],
+                const SizedBox(height: 16.0),
+                ...[
+                  Text(
+                    'Jumlah Iuran',
+                    style: hFont.copyWith(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  TextFormField(
+                    controller: amountController,
+                    style: bFont.copyWith(fontWeight: FontWeight.bold, fontSize: 12.0),
+                    keyboardType: TextInputType.number,
+                    decoration: _inputDecoration.copyWith(prefixText: _currency),
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                    onChanged: (value) {
+                      log("before value $value");
+                      if (value.isNotEmpty) value = _formatNumber(value.replaceAll(",", ""));
+
+                      log("after value value $value");
+                      amountController.value = TextEditingValue(
+                        text: value,
+                        selection: TextSelection.collapsed(offset: value.length),
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 8.0),
                 ],
                 const SizedBox(height: 16.0),
                 ...[
--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -1,6 +1,5 @@
 export './colors.dart';
 export './constant.dart';
 export './fonts.dart';
-export './fonts.dart';
 export './go_router.dart';
 export './shared.dart';
