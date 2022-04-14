6f23237686cd41f3ff124769bfcff8c52ecfeaed
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-04-15 00:11:06 +0700

    Change Application name & remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -38,66 +37,3 @@ Future<void> main() async {
 
 // factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
 // Map<String, dynamic> toJson() => _$UserModelToJson(this);
-
-class Person extends Equatable {
-  const Person({
-    this.name = '',
-  });
-
-  final String name;
-
-  @override
-  List<Object> get props => [name];
-
-  Person copyWith({
-    String? name,
-  }) {
-    return Person(
-      name: name ?? this.name,
-    );
-  }
-
-  static const persons = [
-    Person(name: "Zeffry Reynando"),
-    Person(name: "Helmi Aji Hamamamiardi"),
-    Person(name: "Syarif Hidayatulllah"),
-    Person(name: "Umar Bawazir"),
-    Person(name: "Anggit PP"),
-    Person(name: "Rifda Kamila"),
-    Person(name: "Nakia"),
-    Person(name: "Caul"),
-    Person(name: "Ruth"),
-    Person(name: "Cindy"),
-    Person(name: "Rangga"),
-    Person(name: "Andi S"),
-    Person(name: "Engkoh"),
-  ];
-}
-
-class MyEvent extends Equatable {
-  const MyEvent({
-    this.title = '',
-  });
-
-  final String title;
-
-  @override
-  List<Object> get props => [title];
-
-  @override
-  String toString() => 'MyEvent(title: $title)';
-
-  MyEvent copyWith({
-    String? title,
-  }) {
-    return MyEvent(
-      title: title ?? this.title,
-    );
-  }
-
-  static const myEvents = <MyEvent>[
-    MyEvent(title: "Event 1"),
-    MyEvent(title: "Event 2"),
-    MyEvent(title: "Event 3"),
-  ];
-}
